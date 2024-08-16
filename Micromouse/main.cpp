298d384a394b35f5048bfdc56b635798c1c9180f
Trần Thanh Duy <DUY.TT22714@sinhvien.hsu.edu.vn>
2024-08-16 23:46:04 +0700

    update _sleep()

--- Micromouse/main.cpp
+++ Micromouse/main.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <queue>
 #include <stack>
-
+#include <windows.h>
 #define MAZE_MAX_HEIGHT 16
 #define MAZE_MAX_WIDTH 16
 #define TOP 0
@@ -246,7 +246,7 @@ int main() {
     //     }
     //     // turnRight90();
     //     goStraight();
-    //     _sleep(1000);
+    //     Sleep(1000);
     //     clear_screen();
     // }
     printMazeWithWall();
@@ -905,7 +905,7 @@ void startFloodFill() {
 
         // Simulate a pause
         getchar(); // Pause for input
-        _sleep(500);
+        Sleep(500);
         clear_screen();
     }
 
