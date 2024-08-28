714ace4872d57cfc106f4489d6c09d3b954cf26d
Đinh Quốc Cường <tr6r20@gmail.com>
2024-08-29 02:28:07 +0700

    checkpoint

--- MMS/MMS.ino
+++ MMS/MMS.ino
@@ -9,23 +9,34 @@
 #define L1 9
 #define L2 11
 
+
+
 #define IRLeft A4    // Sharp IR GP2Y0A41SK0F (4-30cm, analog)
 #define IR45Left A3  // Sharp IR GP2Y0A41SK0F (4-30cm, analog)
 #define IRCenter A2
 #define IR45Right A5  // Sharp IR GP2Y0A41SK0F (4-30cm, analog)
-#define IRRight A6
+#define IRRight A7
 
-int speed = 40;
+
+#define RIGHT (-1)  // Sharp IR GP2Y0A41SK0F (4-30cm, analog)
+#define LEFT 1
+
+
+int speed = 80;
+int drift_min = 25;
+int drift_max = 50;
+
 long targetPulses = 1855;  // Số xung cần thiết để đi 16.8 cm, cái cũ là 1670
-Encoder encoderR(encodPinR, encodPinR);
-Encoder encoderL(encodPinL, encodPinL);
+Encoder encoderR(2, 4);
+Encoder encoderL(7, 3);
+
+
+
 float IRLeftValue = 0;
 float IRRightValue = 0;
 float IR45LeftValue = 0;
 float IR45RightValue = 0;
 float IRCenterValue = 0;
-float IR45LeftValue = 0;
-float IR45RightValue = 0;
 
 int flag = 0;
 int flag_distance = 0;
@@ -57,149 +68,12 @@ void setup() {
   Serial.begin(9600);
   // Đặt giá trị khởi tạo của encoder về 0
   initMotor();
-  // lcd.init();
-  // lcd.backlight();
-
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
   pinMode(24, OUTPUT);
-
-  pinMode(encodPinR, INPUT_PULLUP);
-  pinMode(encodPinL, INPUT_PULLUP);
+  digitalWrite(24, HIGH);
 
   encoderR.write(0);
   encoderL.write(0);
-
-  // readDataFromFlash();
 }
-
-void loop() {
-  if (hasWallLeft45()){
-    if (hasWallFront()){
-      if (hasWallRight45()){
-        turnRight180();
-        return;
-      }
-      turnRight90();
-      return;
-    }
-    goStraight();
-    return;
-  }
-  turnLeft90();
-}
-
-void initMotor() {
-  pinMode(L1, OUTPUT);
-  pinMode(L2, OUTPUT);
-
-  pinMode(R1, OUTPUT);
-  pinMode(R2, OUTPUT);
-}
-
-void straight() {
-  analogWrite(R1, speed);
-  analogWrite(R2, 0);
-  analogWrite(L1, speed);
-  analogWrite(L2, 0);
-}
-
-void stop() {
-  analogWrite(R1, 0);
-  analogWrite(R2, 0);
-  analogWrite(L1, 0);
-  analogWrite(L2, 0);
-}
-
-void back() {
-  analogWrite(R1, 0);
-  analogWrite(R2, speed);
-  analogWrite(L1, 0);
-  analogWrite(L2, speed);
-}
-void adjust() {
-
-  // if (theta_degrees > 15.0 && theta_degrees < 10.0) {
-  //   turn(RIGHT);
-  //   delay(60);
-
-  // } else if (theta_degrees < -15.0 && theta_degrees > -10.0) {
-  //   turn(LEFT);
-
-  //   delay(60);
-  // }
-
-  // else if (theta_degrees > 10.0 && theta_degrees < 5.0) {
-  //   turn(RIGHT);
-  //   delay(40);
-
-  // } else if (theta_degrees < -10.0 && theta_degrees > -5.0) {
-  //   turn(LEFT);
-
-  //   delay(40);
-  // }
-
-  //   else
-  if (hasWallLeft() && hasWallRight()) {
-    float theta = atan((getIRRight() - getIRLeft()) / 4.0);
-    float theta_degrees = theta * (180.0 / PI);
-    if (theta_degrees > 15.0) {
-      turn(RIGHT);
-      delay(40);
-    } else if (theta_degrees < -15.0) {
-      turn(LEFT);
-
-      delay(40);
-    }
-  } else if (!hasWallLeft() && hasWallRight()) {
-    float theta = atan((getIRRight() - 6.4) / 4.0);
-    float theta_degrees = theta * (180.0 / PI);
-    if (theta_degrees > 15.0) {
-      turn(RIGHT);
-      delay(40);
-    } else if (theta_degrees < -15.0) {
-      turn(LEFT);
-
-      delay(40);
-    }
-  } else if (hasWallLeft() && !hasWallRight()) {
-    float theta = atan((6.4 - getIRLeft()) / 4.0);
-    float theta_degrees = theta * (180.0 / PI);
-    if (theta_degrees > 15.0) {
-      turn(RIGHT);
-      delay(40);
-    } else if (theta_degrees < -15.0) {
-      turn(LEFT);
-
-      delay(40);
-    }
-  }
-}
-//hàm đi thẳng 1 block
-void goStraight() {
-  // Insert Code IoT Here
-  // Đặt lại giá trị ban đầu của encoder
-  encoderR.write(0);
-  encoderL.write(0);
-
-// // Chạy đến khi đạt đủ số xung
-// while (true) {
-//   long rightPulses = encoderR.read();
-//   long leftPulses = encoderL.read();
-
-    if (rightPulses < targetPulses && leftPulses < targetPulses) {
-      straight();
-    } else {
-      back();
-      delay(20);
-      stop();
-      delay(100);
-      break;  // Dừng lại khi đã đạt mục tiêu
-    }
-  }
-}
-
-
 
 float getIRFront() {
   float center = analogRead(IRCenter) * 0.0048828125;  // Giá trị từ cảm biến * (5V/1024)
@@ -233,39 +107,31 @@ float getIR45Right() {
 
 void goStraight() {
   straight();
-  if (getIR45Left() > 20.0)
-  {
-    stop();
-    delay(5000);
-  }
-  else if (getIR45Left() < 5.0 && !hasWallFront())
-  {
+  if (getIR45Left() < 4.0 && !hasWallFront()) {
     back();
-    delay(20);
+    delay(10);
     stop();
     delay(20);
-    turn(RIGHT);
-    delay(30);
-    turn(LEFT);
+    turnBalance(RIGHT);
+    delay(25);
+    turnBalance(LEFT);
     delay(10);
 
-  }
-  else if (getIR45Left() > 11.5 && !hasWallFront())
-  {
+  } else if (getIR45Left() > 8.5 && !hasWallFront()) {
     back();
-    delay(20);
+    delay(10);
     stop();
     delay(20);
-    turn(LEFT);
-    delay(30);
-    turn(RIGHT);
+    turnBalance(LEFT);
+    delay(25);
+    turnBalance(RIGHT);
     delay(10);
   }
 }
 
 //hàm check sensor đằng trước
 bool hasWallFront() {
-  if (getIRFront() < 16)
+  if (getIRFront() < 8)
     return true;
   return false;
 }
@@ -285,97 +151,149 @@ bool hasWallRight() {
 }
 
 bool hasWallLeft45() {
-  if (getIR45Left() < 14)
+  if (getIR45Left() < 16)
     return true;
   return false;
 }
 
 bool hasWallRight45() {
-  if (getIR45Right() < 14)
+  if (getIR45Right() < 16)
     return true;
   return false;
 }
 
-void turn(int flag_dir) {
+void turnBalance(int flag_dir) {
   if (flag_dir == LEFT) {
-    analogWrite(R1, 0);
+    analogWrite(R1, 40);
     analogWrite(R2, 0);
     analogWrite(L1, 0);
-    analogWrite(L2, speed/2);
+    analogWrite(L2, 40);
   } else {
     analogWrite(R1, 0);
-    analogWrite(R2, speed/2);
-    analogWrite(L1, 0);
+    analogWrite(R2, 40);
+    analogWrite(L1, 40);
     analogWrite(L2, 0);
   }
 }
 
-void turn90(int flag_dir, int countEnc) {
+void initMotor() {
+  pinMode(L1, OUTPUT);
+  pinMode(L2, OUTPUT);
+
+  pinMode(R1, OUTPUT);
+  pinMode(R2, OUTPUT);
+}
+
+void straight() {
+  analogWrite(R1, speed);
+  analogWrite(R2, 0);
+  analogWrite(L1, speed);
+  analogWrite(L2, 0);
+}
+void reverse() {
   back();
   delay(20);
   stop();
   delay(40);
+}
+void stop() {
+  analogWrite(R1, 0);
+  analogWrite(R2, 0);
+  analogWrite(L1, 0);
+  analogWrite(L2, 0);
+}
+
+void back() {
+  analogWrite(R1, 0);
+  analogWrite(R2, speed);
+  analogWrite(L1, 0);
+  analogWrite(L2, speed);
+}
+void drift(int flag_dir) {
+  if (flag_dir == LEFT) {
+    analogWrite(R1, drift_max);
+    analogWrite(R2, 0);
+    analogWrite(L1, drift_min);
+    analogWrite(L2, 0);
+  } else {
+    analogWrite(R1, drift_min);
+    analogWrite(R2, 0);
+    analogWrite(L1, drift_max);
+    analogWrite(L2, 0);
+  }
+}
+void turn(int flag_dir) {
+  drift(flag_dir);
+  delay(150);
+  while (getIR45Left() > 8) {
+    drift(flag_dir);
+    delay(20);
+  }
+  drift(-1 * flag_dir);
+  delay(50);
+  // if (!hasWallLeft()) {
+  //   while (getIRFront() > 12) {
+  //     straight();
+  //   }
+  // }
+  back();
+  delay(40);
+  stop();
+  delay(20);
+}
+void turn90(int flag_dir, int countEnc) {
+  reverse();
   encoderR.write(0);
   encoderL.write(0);
   if (flag_dir == LEFT) {
 
-    turn(flag_dir);
     while (encoderL.read() <= countEnc && encoderR.read() <= countEnc) {
+      turnBalance(flag_dir);
     }
-    turn(RIGHT);
+    turnBalance(RIGHT);
 
   } else if (flag_dir == RIGHT) {
-    turn(flag_dir);
+    digitalWrite(24, LOW);
     while (encoderL.read() <= countEnc && encoderR.read() <= countEnc) {
       // Serial.println(encLeft.read());
-      // Serial.println(encRight.read());
-      Serial.println("trai");
+      // Serial.prencRightintln(.read());
+      turnBalance(flag_dir);
     }
+    turnBalance(LEFT);
+    digitalWrite(24, HIGH);
+  }
+  delay(10);
+  straight();
+  delay(200);
+  reverse();
+}
+void loop() {
+
+  if (!hasWallLeft() && getIRFront() < 8) {
+    reverse();
+    speed = 40;
+    turn90(LEFT, 620);
+    return;
+  }
+  if (!hasWallLeft45()) {
+    reverse();
+    speed = 40;
     turn(LEFT);
+    return;
   }
-  delay(40);
-  stop();
-  delay(150);
-  // flag_quay = 1;
-}
-
-//hàm quay xe về trái 90*
-void turnLeft90() {
-
-  switch (mouseDirection) {
-    case 0:
-      mouseDirection = 2;
-      break;
-    case 1:
-      mouseDirection = 3;
-      break;
-    case 2:
-      mouseDirection = 1;
-      break;
-    case 3:
-      mouseDirection = 0;
-      break;
-    default:
-      break;
+  if (!hasWallRight45() && getIRFront() < 8) {
+    reverse();
+    speed = 40;
+    turn(RIGHT);
+    return;
   }
-  // Insert Code IoT Here
-  turn90(LEFT, 640);
-}
-
-//hàm quay xe về phải 90*
-void turnRight90() {
-  // Insert Code IoT Here
-  turn90(RIGHT, 640);
-}
-
-//hàm quay đầu theo bên trái
-void turnLeft180() {
-  // Insert Code IoT Here
-  turn90(RIGHT, 1360);
-}
-
-//hàm quay đầu theo bên phải
-void turnRight180() {
-  // Insert Code IoT Here
-  turn90(RIGHT, 1360);
+  if (!hasWallRight() && getIRFront() < 8) {
+    reverse();
+    speed = 40;
+    turn90(RIGHT, 620);
+    return;
+  }
+  ifd
+  speed = 80;
+  goStraight();
 }
